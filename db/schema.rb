9a982ca29f4309bda6ea7109bb4dd65bab47c06e
Tony Baker <this.tonybaker@gmail.com>
2019-08-02 01:47:36 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190802014521) do
+ActiveRecord::Schema.define(version: 20190802014726) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20190802014521) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "passenger_id"
+    t.integer  "taxi_id"
   end
 
   create_table "taxis", force: :cascade do |t|
